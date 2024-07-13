c9c9795228eae306275638e07d2f28a690fdf837
VIDESH MAKWANA <videsh.m@brilworks.com>
2024-07-13 12:08:29 +0530

    test

---
+++ src/main/java/vdmcreation/com/AWSS3ConnectSpringBoot/Awss3ConnectSpringBootApplication.java
@@ -0,0 +1,13 @@
+package vdmcreation.com.AWSS3ConnectSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Awss3ConnectSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Awss3ConnectSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vdmcreation/com/AWSS3ConnectSpringBoot/Awss3ConnectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package vdmcreation.com.AWSS3ConnectSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Awss3ConnectSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
